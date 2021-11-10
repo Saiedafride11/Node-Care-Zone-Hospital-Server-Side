870b7d59574d99fb0ced843df74bc78a7ae52692
Dark coders <mdsaied.afride@gmail.com>
2021-11-10 00:39:52 -0800

    toLocalDateString

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ async function run() {
      
       app.get('/appointments', verifyToken, async (req, res) => {
         const email = req.query.email;
+        // const date = new Date(req.query.date).toLocalDateString();
         const date = req.query.date;
 
         const query = { email: email, date: date }
